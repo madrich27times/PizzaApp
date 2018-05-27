1eca907de0e46cbd1a10c9162d4529df8c95bc26
Maddy Richard <MRichard@student.neumont.edu>
2018-05-27 14:20:26 -0600

    Working on order pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 var filesToLoad = [];
 var loadedJSON = [];
 var pages = [
-  "<div class='row'>      <div class='col-4'>        <h1 class='bold-heading title'>Business Name</h1>      </div>      <div class='col-4'></div>      <div class='col-4'></div>    </div>    <div class='row'>      <div id='byoBtn' class='col-6 card'>        <div class='card-container'>          <div class='card-header'>            <h1 class='bold-heading'>Build Your Own</h1>          </div>          <div class='card-body box'>Pizza Image</div>        </div>      </div>      <div id='preBtn' class='col-6 card'>        <div class='card-container'>          <div class='card-header'>            <h1 class='bold-heading'>Pre-Built Pizza</h1>          </div>          <div class='card-body box'>Pizza Image</div>        </div>      </div>    </div>"
+  "<div class='row'>      <div class='col-4'>        <h1 class='bold-heading title'>Business Name</h1>      </div>      <div class='col-4'></div>      <div class='col-4'></div>    </div>    <div class='row'>      <div id='byoBtn' class='col-6 card'>        <div class='card-container'>          <div class='card-header'>            <h1 class='bold-heading'>Build Your Own</h1>          </div>          <div class='card-body box'>Pizza Image</div>        </div>      </div>      <div id='preBtn' class='col-6 card'>        <div class='card-container'>          <div class='card-header'>            <h1 class='bold-heading'>Pre-Built Pizza</h1>          </div>          <div class='card-body box'>Pizza Image</div>        </div>      </div>    </div>",
+  "<div class='row'>      <div class='col-4'>        <h1 class='bold-heading title'>Pre-Built Options</h1>      </div>      <div class='col-4'></div>      <div class='col-4'></div>    </div>    <div class='row'>      <div class='col-4'>        <div class='popup box'>Pizza Image          <span class='popup-content'>Pizza Description</span>        </div>      </div>      <div class='col-4'>        <div class='popup box'>Pizza Image          <span class='popup-content'>Pizza Description</span>        </div>      </div>      <div class='col-4'>        <div class='popup box'>Pizza Image          <span class='popup-content'>Pizza Description</span>        </div>      </div>    </div>    <div class='row'>      <div class='col-6'>        <div class='popup box'>Pizza Image          <span class='popup-content'>Pizza Description</span>        </div>      </div>      <div class='col-6'>        <div class='popup box'>Pizza Image          <span class='popup-content'>Pizza Description</span>        </div>      </div>    </div>    <div class='row button-row'>      <div class='col-2'>        <div id='backBtn' class='btn-basic'>Back</div>      </div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>            <div class='col-1'>        <div id='nextBtn' class='btn-basic'>Next</div>      </div>    </div>",
+  "<div class='row'>      <div class='col-4'>        <h1 class='bold-heading title'>Choose your size</h1>      </div>      <div class='col-4'></div>      <div class='col-4'></div>    </div>    <div class='row'>      <div class='col-3 size-container'>        <div id='smallPizza' class='size-box'>S        </div>      </div>      <div class='col-3 size-container'>        <div id='mediumPizza' class='size-box'>M        </div>      </div>      <div class='col-3 size-container'>        <div id='largePizza' class='size-box'>L        </div>      </div>      <div class='col-3 size-container'>        <div id='xLPizza' class='size-box'>XL        </div>      </div>    </div>    <div class='row button-row'>      <div class='col-2'>        <div id='backBtn' class='btn-basic'>Back</div>      </div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'></div>      <div class='col-1'>        <div id='nextBtn' class='btn-basic'>Next</div>      </div>    </div>"
 ];
 var currentPage = 0;
+var isPopup = false;
+var isModal = false;
+var isDropdownList = false;
 
 function loadJSON(filename, callback) {
   var xobj = new XMLHttpRequest();
@@ -32,7 +37,7 @@ function elementsInit() {
 function changePage(pageNum) {
   console.log("pageNum ", pageNum);
   currentPage = pageNum;
-  
+
   // switch (pageNum) {
   //   case 0:
   //   buildStartPage();
@@ -43,6 +48,16 @@ function changePage(pageNum) {
   //   break;
   // }
   document.getElementById("pizza").innerHTML = pages[pageNum];
+}
+
+function back() {
+  var back = currentPage - 1;
+  changePage(back);
+}
+
+function next() {
+  var next = currentPage + 1;
+  changePage(next);
 }
 
 function buildStartPage() {
@@ -78,7 +93,7 @@ function buildStartPage() {
 
   var conCol1 = document.createElement("div");
   conCol1.className = "col-6";
-  conCol1.classList += " card";  
+  conCol1.classList += " card";
   conCol1.id = "byoBtn";
   var cardContainer1 = document.createElement("div");
   cardContainer1.className = "card-container";
@@ -119,7 +134,7 @@ function buildStartPage() {
   PBimg.src = "images/";
   cardBody2.appendChild(PBImg);
   cardContainer2.appendChild(cardBody2);
-  conCol2.appendChild(cardContainer2);  
+  conCol2.appendChild(cardContainer2);
   contentRow.appendChild(conCol2);
 
   container.appendChild(contentRow);
@@ -132,13 +147,13 @@ function modalInit() {
     var close = document.getElementsByClassName("close");
 
     for (let i = 0; i < modalBtns.length; i++) {
-      modalBtns[i].addEventListener("click", function() {
+      modalBtns[i].addEventListener("click", function () {
         var modal = modalBtns[i].getAttribute("data-modal");
         document.getElementById(modal).style.display = "block";
       });
     }
     for (let j = 0; j < close.length; j++) {
-      close[j].addEventListener("click", function() {
+      close[j].addEventListener("click", function () {
         var modal = close[j].closest(".modal");
         modal.style.display = "none";
       });
@@ -150,10 +165,8 @@ function popupInit() {
   if (isPopup == true) {
     var popups = document.getElementsByClassName("popup");
     for (let i = 0; i < popups.length; i++) {
-      popups[i].addEventListener("click", function() {
-        console.log(popups[i]);
-        // var content = document.getElementsByClassName("popup-content");
-        // content[i].classList.toggle("show");
+      popups[i].addEventListener("click", function () {
+        //console.log(popups[i]);
         var popupContent = this.firstElementChild;
         if (popupContent.style.visibility == "visible") {
           popupContent.style.visibility = "hidden";
@@ -165,7 +178,26 @@ function popupInit() {
   }
 }
 
+function dropdownListInit() {
+  if (isDropdownList == true) {
+    var dropdownLists = document.getElementsByClassName("dropdown-list");
+    for (let i = 0; i < dropdownLists.length; i++) {
+      dropdownLists[i].addEventListener("click", function () {
+        var content = document.getElementsByClassName("dropdown-list-contents");
+        if (content[i].style.visibility == "visible") {
+          content[i].style.visibility = "hidden";
+        } else {
+          content[i].style.visibility = "visible";
+        }
+      });
+    }
+
+  }
+}
+
 // var runJSON = setTimeout(init(), 3000);
 //changePage(0);
-var isPopup = true;
-popupInit();
+// var isPopup = true;
+// popupInit();
+var isDropdownList = true;
+dropdownListInit();
